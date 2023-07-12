85eb6d30b441c56d3c4250f51cdf16540463c452
shashu22 <challasaisashank@gmail.com>
2023-07-12 11:31:10 -0500

    added functionality

---
+++ rest/src/main/java/com/demoapp/rest/SubmissionForm.java
@@ -0,0 +1,59 @@
+package com.demoapp.rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class SubmissionForm {
+    public SubmissionForm(String name, String vendorName, int rate, String leadName, String technology) {
+        this.Name=name;
+        this.rate=rate;
+        this.leadName=leadName;
+        this.setTechnology(technology);
+        this.setVendorName(vendorName);
+
+    }
+    public SubmissionForm(){
+
+    }
+
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setVendorName(String vendorName) {
+        VendorName = vendorName;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setLeadName(String leadName) {
+        this.leadName = leadName;
+    }
+
+    public void setTechnology(String technology) {
+        Technology = technology;
+    }
+
+    public String Name;
+
+    @JsonIgnore
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String Id;
+    public String VendorName;
+    public int rate;
+    public String leadName;
+    public String Technology;
+
+
+
+}
